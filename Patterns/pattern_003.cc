4a91c4a6360f1d16d87c0da0b5d077c2338a9fb3
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-09-06 21:14:25 +0530

    Create pattern_003.cc

---
+++ Patterns/pattern_003.cc
@@ -0,0 +1,24 @@
+/*
+    1 
+    1 2 
+    1 2 3
+    1 2 3 4 
+    1 2 3 4 5
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cout<<"Enter the value of N : ";
+    cin>>N;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
