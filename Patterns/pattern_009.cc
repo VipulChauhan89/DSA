66db061af9c3d044e683ad3f727043992cea3c8b
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-09-07 20:23:39 +0530

    Create pattern_009.cc

---
+++ Patterns/pattern_009.cc
@@ -0,0 +1,49 @@
+/*
+            *
+          * * * 
+        * * * * * 
+      * * * * * * * 
+    * * * * * * * * *
+    * * * * * * * * *
+      * * * * * * * 
+        * * * * * 
+          * * * 
+            *
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cout<<"Enter the value of N : ";
+    cin>>N;
+    for(int i=1;i<=N;i++)
+    {
+        int k=0;
+        for(int j=i;j<N;j++)
+        {
+            cout<<"  ";
+        }
+        while(k<2*i-1)
+        {
+            cout<<"* ";
+            k++;
+        }
+        cout<<endl;
+    }
+    for(int i=N;i>0;i--)
+    {
+        int k=0;
+        for(int j=i;j<N;j++)
+        {
+            cout<<"  ";
+        }
+        while(k<2*i-1)
+        {
+            cout<<"* ";
+            k++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
