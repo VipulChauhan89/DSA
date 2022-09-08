e2d12676ef621a6b5650c24e36f49067c24cf7d8
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-09-08 19:45:12 +0530

    Create pattern_014.cc

---
+++ Patterns/pattern_014.cc
@@ -0,0 +1,30 @@
+/*
+    A 
+    A B
+    A B C
+    A B C D 
+    A B C D E
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N,k;
+    cout<<"Enter the value of N between 1 to 26 : ";
+    cin>>N;
+    if(N<=0 || N>26)
+    {
+        cout<<"You did not entered the right number.\n";
+        return 0;
+    }
+    for(int i=0;i<N;i++)
+    {
+        k=65;
+        for(int j=0;j<=i;j++)
+        {
+            cout<<(char)k++<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
